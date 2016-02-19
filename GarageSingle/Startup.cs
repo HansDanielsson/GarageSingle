0337a80c8f9a0d3d5ba665645bdce8bfbece7913
HansDanielsson <Hans.Ro.Danielsson@gmal.com>
2016-02-19 09:21:09 +0100

    GarageSingle start

---
+++ GarageSingle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GarageSingle.Startup))]
+namespace GarageSingle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
